5febf5331afeeb286c971f779f26ebc1940599bd
GopichandGutta <gopichandgutta17@gmail.com>
2022-04-01 21:32:28 +0530

    emp

---
+++ Emp_No.java
@@ -0,0 +1,13 @@
+Emp_No|Emp_Name|Salary|Dept_No|Location
+101|Sagar Sawant|15000|10|Indore
+102|Mona Chaurasia|18039|50|Chennai
+103|Rahul Jujum|23000|20|Pune
+104|Shilpa Chimad|27024|10|Mumbai
+105|Amol Deshmukh|40498|50|Mumbai
+106|Divyasree Kandrapa|12035|20|Vijayawada
+107|Harshavardhan Chowdary|34023|10|Guntur
+108|Pankaj Singh|24000|40|Mumbai
+109|Himanshu Mishra|23593||20Shimla
+110|Jojo Yohonnan|24950|30|Trivandrum
+111|Manisha Tiwari|24958|20|Mumbai
+112|Gaurav Takle|25000|40|Pune
